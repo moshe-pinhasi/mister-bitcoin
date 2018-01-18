fdb910af2a8e8ec3894f94c87d1bb71c2c9aae3b
moshe-pinhasi <pi.moshe@gmail.com>
2018-01-18 13:42:25 +0200

    using the hashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { loadUser } from './actions/user.actions'
